fc3aa38a2a500439cb967750062e58069d5374a7
nayeong lee <lny021102@gmail.com>
2024-02-25 23:59:58 +0900

    [ADD] 조건문으로 사진 추가

--- src/pages/HeaderPages/Introduction/Components/1.Introduction/Introduction.jsx
+++ src/pages/HeaderPages/Introduction/Components/1.Introduction/Introduction.jsx
@@ -1,8 +1,15 @@
-// import styled from 'styled-components';
+import styled from 'styled-components';
 import ContainerColumn from '../../../../../Components/Container/ContainerColumn';
 import IntroductionTexts from './IntroductionTexts';
 import { TextH1, TextP } from '../Text';
 import Title from '../Title';
+
+const CustomStyledImg = styled.img`
+    width: ${props => props.width || 'auto'};
+    height: ${props => props.height || 'auto'};
+    border-radius: 20px;
+    margin: 10px;
+`;
 
 export default function Introduction() {
 
@@ -15,6 +22,11 @@ export default function Introduction() {
                     {section[1].map((paragraph, pIndex) => (
                         <TextP key={pIndex}>{paragraph}</TextP>
                     ))}
+                    {index === 0 && <CustomStyledImg src={'/img/intro_img2.jpg'} width='50%' height='30vh' />}
+                    {index === 1 && <CustomStyledImg src={'/img/intro_img4.jpg'} width='50%' height='30vh' />}
+                    {index === 2 && <CustomStyledImg src={'/img/intro_img1.jpg'} width='50%' height='30vh' />}
+                    {index === 3 && <CustomStyledImg src={'/img/intro_img3.jpg'} width='50%' height='30vh' />}
+                    {index === 4 && <CustomStyledImg src={'/img/intro_img5.jpg'} width='50%' height='30vh' />}
                 </div>
             ))}
         </ContainerColumn>
